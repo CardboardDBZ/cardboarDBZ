91dd58fbbf1ab38a29fc1fc85e2648977ac98112
Jay Hack <jhack@stanford.edu>
2014-10-03 23:37:02 -0700

    setup_environment now configured

--- PrimeSense/setup_environment.sh
+++ PrimeSense/setup_environment.sh
@@ -1,5 +1,8 @@
-export OPENNI2_PATH='../../Open/'
-export NITE2_PATH='../../OpenNI2/'
+export OPENNI2_PATH='../../OpenNI2'
+export NITE2_PATH='../../NiTE2/'
 source $OPENNI2_PATH/install.sh
 source $NITE2_PATH/install.sh
+source $OPENNI2_PATH/OpenNIDevEnvironment
+source $NITE2_PATH/NiTEDevEnvironment
+
 
