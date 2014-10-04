1389855ac614170dae22b0b3a426c71ce1ebc5f9
Jay Hack <jhack@stanford.edu>
2014-10-04 02:33:43 -0700

    TCP ports now; starting to debug

--- PrimeSense/PrimeSense.py
+++ PrimeSense/PrimeSense.py
@@ -6,8 +6,7 @@
 import numpy as np
 import scipy as sp
 import pandas as pd
-import primesense
-from primesense import openni2, nite2
+import zmq
 
 def print_status(message):
 	print '---> %s' % message
@@ -32,10 +31,21 @@ class PrimeSense:
 		print_status('Initializing OpenNI2')
 		openni2.initialize()
 		print_status('Initializing NiTE2')
-		nite2.initialize()
+		nite2.initialize(dll_directories=['/Users/jayhack/CS/NI/NiTE2/Redist'])
+
+		#=====[ Step 2: get the device	]=====
+		self.device = openni2.Device.open_any()
+
+
+	def zmq_init(self):
+		"""
+			initializes zeromq for receiving/sending frames
+		"""
 
 
 if __name__ == '__main__':
 
 	p = PrimeSense()
+
+
 
