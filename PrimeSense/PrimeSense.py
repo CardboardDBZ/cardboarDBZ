db3218ffa28df1ad0012d39baa56cd1f4308ba72
Jay Hack <jhack@stanford.edu>
2014-10-04 14:33:16 -0700

    this is now correctly tracking two players that appear on screen

--- PrimeSense/PrimeSense.py
+++ PrimeSense/PrimeSense.py
@@ -3,14 +3,19 @@
 # -----------------
 # wrapper class for dealing with the primesense
 ####################
-from collections import defaultdict
+import os
+import pickle
+from copy import copy
 import numpy as np
 import scipy as sp
 import pandas as pd
 import zmq
+
 from StoppableThread import StoppableThread
 from DeviceReceiver import DeviceReceiver
 from CommunicationHost import CommunicationHost
+from Player import Player
+
 
 def print_status(message):
 	print '---> %s' % message
@@ -32,14 +37,22 @@ class PrimeSense():
 			p.skeleton_poses_h: list of dfs reprsenting poses in h_coords
 	"""
 
-	def __init__(self):
+	def __init__(self, video=None):
 
 		#=====[ Step 1: setup receiving data	]=====
-		self.receiver = DeviceReceiver('primesense')
+		if video:
+			self.video = video			
+			self.video_mode = True
+			self.video_frame = -1
+		else:
+			self.receiver = DeviceReceiver('primesense')
+			self.video_mode = False
+
+		#=====[ Step 2: setup communication	]=====
 		self.communication_host = CommunicationHost()
 
-		#=====[ Step 2: set up coordinate lists	]=====
-		self.joint_frames = defaultdict(list)
+		#=====[ Step 3: try to inialize the game	]=====
+		self.init_game ()
 
 
 
@@ -48,15 +61,6 @@ class PrimeSense():
 			ex: JOINT_HEAD_POSITION -> head
 		"""
 		return '_'.join(key_name.split('_')[1:-1]).lower()
-
-
-	def get_pos_df(self):
-		"""
-			sets self.pos_df from self.x_positions... etc
-
-		"""
-		dfs = [pd.DataFrame(x) for x in self.joint_frames.values()]
-		self.pos_df = pd.concat(dfs, keys=self.joint_frames.keys(), axis=1)
 
 
 	def dict_to_df(self, d):
@@ -73,18 +77,31 @@ class PrimeSense():
 		return pd.DataFrame(d)
 
 
-	def update(self, input_frame=None):
+	def update_skeletons(self, input_frame=None):
 		"""
-			- updates frame_processed (primesense coordinates)
+			updates the following variables:
+				- frame_raw: raw json 
+				- num_skeletons: # of skeletons
+				- frame_processed: cleaned json
+				- skeleton_poses_c: list of dataframes containing skeleton poses (c_coords)
+				- skeleton_poses_h: list of dataframes containing skeleton poses (h_coords)
 		"""
-		#=====[ Step 1: raw frame and metadata	]=====
+		#=====[ Step 1: deal with video mode	]=====
+		if not input_frame and self.video_mode:
+			self.video_frame += 1
+			raw_input('Video Mode [%s]: press enter to continue to next frame' % self.video_frame)
+			self.update_skeletons(input_frame=self.video[self.video_frame])
+			self.print_game_state()
+			return
+
+		#=====[ Step 2: figure out what the input frame is	]=====
 		if input_frame:
 			self.frame_raw = input_frame
 		else:
 			self.frame_raw = self.receiver.get_frame()
 		self.num_skeletons = len(self.frame_raw.keys())
 
-		#=====[ Step 2: process the frame	]=====
+		#=====[ Step 3: process the frame	]=====
 		self.frame_processed = {}
 		for s_name, s_frame in self.frame_raw.iteritems():
 			self.frame_processed[s_name] = {}
@@ -93,95 +110,87 @@ class PrimeSense():
 					key = self.key_conversion(raw_key)
 					self.frame_processed[s_name][key] = s_frame[raw_key]
 
-		#=====[ Step 3: get skeleton_poses	]=====
+		#=====[ Step 4: get skeleton_poses	]=====
 		self.skeleton_poses_c = []
 		for s_name, s_frame in self.frame_processed.items():
 			self.skeleton_poses_c.append(self.dict_to_df(s_frame))
 
-		#=====[ Step 4: get skeleton_poses_h	]=====
-		self.skeleton_poses_h = [self.to_human_coords(c_coords) for c_coords in self.skeleton_poses_c]
+
+	def init_game(self):
+		"""
+			Tries to initialize the two players; won't do so 
+			until there are at least two players present
+		"""
+		self.game_started = False
+		self.players = [None, None]
+		self.update_skeletons()
+
+		#=====[ Step 1: loop until we see two players	]=====
+		while (self.num_skeletons < 2):
+			self.update_skeletons()
+
+		#=====[ Step 2: initialize the two players	]=====
+		head_coords = [c_coords['head']['x'] for c_coords in self.skeleton_poses_c]
+		leftmost_player_ix = np.argmin(head_coords) #we THINK this gets the leftmost
+		rightmost_player_ix = np.argmax(head_coords)
+		print leftmost_player_ix, rightmost_player_ix
+		print head_coords
+		self.players[0] = Player(self.skeleton_poses_c[leftmost_player_ix])
+		self.players[1] = Player(self.skeleton_poses_c[rightmost_player_ix])
+
+
+	def update_players(self):
+		"""
+			for each player, this will have them choose the 
+			best new location for their skeleton 
+		"""
+		skeleton_c_coords = copy(self.skeleton_poses_c)
+		for player in self.players:
+			player.update(skeleton_c_coords)
+
+
+	def update_game(self):
+		"""
+			updates all player locations in the game;
+		"""
+		self.update_skeletons()
+		self.update_players()
+		self.print_game_state()
+		# self.notify_players() #TODO
+
+
+	def print_game_state(self):
+		"""
+			prints out what is going on in the game currently
+		"""
+		print '##################################################'
+		print '##########[ FRAME STATS ]#########################'
+		print '##################################################'				
+		print '# skeletons in frame: %d' % self.num_skeletons
+
+		print '\n=====[ PLAYER 1	]====='
+		print self.players[0]
+
+		print '\n=====[ PLAYER 2	]====='
+		print self.players[1]
 
 
 
 	def record(self):
 		print "===[ Press Enter to Start ]==="
 		raw_input('--->')
+		raw_frames = []
 		while True:
 			try:
-				self.update()
+				self.update_skeletons()
+				raw_frames.append(self.frame_raw)
 			except KeyboardInterrupt:
 				break
 		print '===[ Finished Recording ]==='
-
-
-	def person_exists(self):
-		"""
-			returns true if there is a person onscreen in the last 
-			frame in self.pos_df
-			criterion: all *positions* are non-null
-		"""
-		return self.pos_df.iloc[-1].isnull().sum() == 0
-
-
-
-	def get_origin_axes(self, c_coords):
-		"""
-			given a body represented in camera coordinates,
-			returns its origin,x,y,z_axes
-
-			c_coords: pd.DataFrame containing camera coordinates
-			returns: numpy arrays for origin, x/y/z axes
-		"""
-		origin = np.array((c_coords['left_shoulder'] + c_coords['right_shoulder'])/2.)
-		x_axis = np.array(c_coords['right_shoulder'] - origin)
-		z_axis = np.array(origin - c_coords['torso'])
-		y_axis =np.cross(z_axis, x_axis)
-		x_axis = x_axis / x_axis.sum()
-		y_axis = y_axis / y_axis.sum()
-		z_axis = z_axis / z_axis.sum()
-		return origin, x_axis, y_axis, z_axis
-
-
-	def convert_coordinate_system(self, original_coords, origin, x_axis, y_axis, z_axis):
-		"""
-			converts original_coords to the coordinate system 
-			represented by origin, x_axis, y_axis, z_axis. each of those 
-			are in the original cooords.
-
-		"""
-		#=====[ Step 1: get differences	]=====
-		differences = original_coords.copy()
-		for label in original_coords.columns:
-			differences[label] = differences[label] - origin
-
-		#=====[ Step 2: transform each one	]=====
-		M = np.eye(3)
-		M[:, 0], M[:,1], M[:,2] = x_axis, y_axis, z_axis
-		# M*[a, b, c] = d
-		# ==> pinv(M)*d = [a, b, c]
-		new_coords = differences.copy()
-		for label in new_coords.columns:
-			new_coords[label] = np.dot(np.linalg.pinv(M), new_coords[label])
-		return new_coords
-
-
-	def to_human_coords(self, camera_coords):
-		"""
-			given a position df, this will return 
-			it transformed
-
-			kinect_coords: df of coordinates from the primesense's 
-				perspective
-
-			Uses convention: c = camera, h = human
-
-		"""
-		origin, x_axis, y_axis, z_axis = self.get_origin_axes(camera_coords)
-		return self.convert_coordinate_system(camera_coords, origin, x_axis, y_axis, z_axis)
-
-
-		
-
+		print "ENTER SAVE NAME: (in ./data/)"
+		save_name = raw_input('--> ')
+		pickle.dump(raw_frames, open(os.path.join('./data', 'save_name'), 'w'))
+		return raw_frames
 
 
 
