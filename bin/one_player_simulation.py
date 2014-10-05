6d6f3e747ebfc0e4d0f4e906c995d73654b67488
Jay Hack <jhack@stanford.edu>
2014-10-05 04:21:24 -0700

    now allowing for a single player; script in bin

--- bin/one_player_simulation.py
+++ bin/one_player_simulation.py
@@ -3,8 +3,8 @@ from DBZHost import DBZController
 
 if __name__ == '__main__':
 
-	sample_video = pickle.load(open('../data/lucas_vs_brandon.vid','r'))
-	controller = DBZController(num_players=2, video=sample_video, data_dir='../data')
+	sample_video = pickle.load(open('../data/jay_alone.vid','r'))
+	controller = DBZController(num_players=1, video=sample_video, data_dir='../data')
 	for i in range(len(sample_video) - 2):
 		controller.update_game()
 
