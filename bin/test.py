d8c80af66f0622a58ef7b4513273bb6ed8f443db
Jay Hack <jhack@stanford.edu>
2014-10-04 18:44:05 -0700

    removed old classifier

--- bin/test.py
+++ bin/test.py
@@ -1,10 +1,10 @@
 import pickle
-from PrimeSense import PrimeSense
+from DBZHost import DBZController
 
 if __name__ == '__main__':
 
-	sample_video = pickle.load(open('./data/twoplayer_test_1.pkl','r'))
-	p = PrimeSense(sample_video)
+	sample_video = pickle.load(open('../data/twoplayer_test_1.pkl','r'))
+	p = DBZController(sample_video)
 	for i in range(len(sample_video) - 2):
 		p.update_game()
 
